834387cea085863b23c54fe911c5f2c0a81d153b
MohdIrfan03 <168664403+MohdIrfan03@users.noreply.github.com>
2026-05-17 00:27:35 +0530

    Practiced JavaScript functions

---
+++ javascript/practise.js
@@ -0,0 +1,40 @@
+/* add two numbers*/
+// function add(a,b){
+//     return (a+b);
+// }
+// console.log(add(2,3));
+
+// let addd=function(a,b){return a+b;};
+// console.log(addd(3,6));
+
+// let arr=(a,b) => a+b;
+// console.log(arr(7,8));
+
+// check even odd
+// number=16;
+// function check(number){
+//     if (number%2===0){
+//         console.log("its even");
+//     }
+//     else{
+//         console.log("its odd");
+//     }
+// }
+// check(number);
+
+// let checkkk=n => {if (n%2==0){
+//     console.log("even");
+// }else{cosole.log("odd");}};checkkk(8);
+
+// find largest nuber
+// const nums = [3, 7, 2, 9, 1];
+// let largest = nums[0];
+// for (let i=1;i<nums.length;i++){
+//     if (nums[i]>largest){
+//         largest=nums[i];
+//     }
+// }
+// console.log(largest);
+
+// const findLargest = (arr) => Math.max(...arr);
+// console.log(findLargest([3, 7, 2, 9, 1])); 
